2ada633fe383efd2b40d7461ada269eb225d20de
Sebastian Lozada <sebaslozaare@gmail.com>
2024-05-02 23:08:27 -0600

    version 2

--- MainPersona.java
+++ MainPersona.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
-class MainPersona{
+
+class MainPersona {
     public static void main(String[] args) {
-    Scanner op= new Scanner(System.in);
-    String nombre;
-    int edad;
-    double altura;
-    Persona x= new Persona();
-    
-    System.out.println("INGRESE SU NOMBRE: ");
-    nombre=op.next();
-    x.nombre= nombre;
-    System.out.println("INGRESE SU EDAD: ");
-    edad=op.nextInt();
-    x.edad= edad;
-    System.out.println("INGRESE SU ALTURA: ");
-    altura=op.nextDouble();
-    x.altura= altura;
-    
-    x.mostrarNombre();
-    x.mostrarEdad();
-    x.mostrarAltura();
+        Scanner op = new Scanner(System.in);
+        String nombre, apellido;
+        int edad;
+        double altura;
+        Persona x = new Persona();
+
+        System.out.println("INGRESE SU NOMBRE: ");
+        nombre = op.next();
+        x.setNombre(nombre);
+        System.out.println("INGRESE SU Apellido: ");
+        apellido = op.next();
+        x.setApellido(apellido);
+        System.out.println("INGRESE SU EDAD: ");
+        edad = op.nextInt();
+        x.setEdad(edad);
+        System.out.println("INGRESE SU ALTURA: ");
+        altura = op.nextDouble();
+        x.setAltura(altura);
+
+        System.out.println(x);
 
     }
 }
--- Persona.java
+++ Persona.java
@@ -1,15 +1,68 @@
-class Persona{
-    String nombre, apellido;
-    int edad;
-    double altura;
+class Persona {
+    private String nombre, apellido;
+    private int edad;
+    private double altura;
+
+    @Override
+    public String toString() {
+        return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", altura=" + altura + "]";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad > 0) {
+            this.edad = edad;
+
+        } else {
+            System.out.println("edad invalidad");
+        }
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura > 0) {
+            this.altura = altura;
+
+        } else {
+            System.out.println("altura invalida");
+        }
+    }
 
     public void mostrarNombre() {
-        System.out.println("Mi nombre es: "+this.nombre);
+        System.out.println("Mi nombre es: " + this.nombre);
     }
+
+    public void mostrarApellido() {
+        System.out.println("Mi apellido es: " + this.apellido);
+    }
+
     public void mostrarEdad() {
-        System.out.println("Mi edad es: "+this.edad);
+        System.out.println("Mi edad es: " + this.edad);
     }
+
     public void mostrarAltura() {
-        System.out.println("Mi altura es: "+this.altura);
+        System.out.println("Mi altura es: " + this.altura);
     }
 }
